2486b7c00b47b6b476bf62f3974472396ed1a074
FJ9019 <fenelus24@gmail.comgit config --global user.email fenelus24@gmail.comgit config --global user.email fenelus24@gmail.com>
2025-03-31 14:09:36 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.stacklayout import StackLayout
+
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+
+class MainWidget(Widget):
+    pass
+
+class CanvasTest(Widget):
+    pass
+
+class MainApp(App):
+    pass
+
+app = MainApp()
+app.run()
