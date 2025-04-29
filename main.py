cbfa8b03c7e4a5f4a5a999a168bf99bfe34b769d
FJ9019 <fenelus24@gmail.comgit config --global user.email fenelus24@gmail.comgit config --global user.email fenelus24@gmail.com>
2025-04-29 12:44:33 -0400

    Animation Ball

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 
+from kivy.graphics import Ellipse
+from kivy.metrics import dp
+
 class MainWidget(Widget):
     pass
 
 class CanvasTest(Widget):
+    pass
+
+class AnimationBall(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.vy = dp(5)
+        with self.canvas:
+            self.ball = Ellipse(pos=(dp(350), 0), size=(dp(50), dp(50)))
     pass
 
 class MainApp(App):
